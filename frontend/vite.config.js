8a1dbf1a190371467f3a19c2cc20f047a9bea752
mariana142006 <mg5002006@gmail.com>
2026-09-07 09:41:20 -0500

    Update allowed hosts in Vite config
    Added new allowed host for preview and server.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   preview: {
     host: true,
-    allowedHosts: ["selfless-cat-production-4cac.up.railway.app"],
+    allowedHosts: ["selfless-cat-production-4cac.up.railway.app", "app.colsanpedroclavertulua.edu.co"],
   },
+  server: {
+    host: true,
+    allowedHosts: ["selfless-cat-production-4cac.up.railway.app", "app.colsanpedroclavertulua.edu.co"],
+  }
 });
